48ee4f44ed578c7014a44176d1ec01b952b426df
Sofronijev <sofronijev@gmail.com>
2020-02-27 19:57:08 +0100

    Updates

---
+++ precache-manifest.2a351958c9da825dc7ad1723ff5b117e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03b3e4dbf79051699562b45b69a56d9f",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "2ef75c4c7d61c8e5a5ae",
+    "url": "/react-calculator/static/css/main.1ebf9721.chunk.css"
+  },
+  {
+    "revision": "72e12128ee32f6029125",
+    "url": "/react-calculator/static/js/2.13881121.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-calculator/static/js/2.13881121.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ef75c4c7d61c8e5a5ae",
+    "url": "/react-calculator/static/js/main.4db157eb.chunk.js"
+  },
+  {
+    "revision": "b0b7cd89c16c74fca524",
+    "url": "/react-calculator/static/js/runtime-main.c742ce0b.js"
+  }
+]);
